ba6a70d7efb2af55b7fe70bd8f672b8466391154
星冉 <Jalapeno1868@outlook.com>
2024-11-24 22:42:23 +0800

    fix-server

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-@echo off && chcp 65001
+chcp 65001
 
 cls
 
@@ -16,15 +16,11 @@ if not exist "mdm-linux-amd64" (
     echo Build Server Done
 )
 
-call :chmodT  mdm-linux-amd64
-
 :: buildClient
 if not exist "mdm-darwin-amd64" (
     xgo --targets=darwin/amd64,darwin/arm64 -ldflags="-s -w -extldflags -static" ./mdm
     echo Build Client Done
 )
-
-call :chmodT mdm-linux-amd64
 
 :: pkgObfuscate
 if not exist "server\bash-obfuscate" (
@@ -32,17 +28,22 @@ if not exist "server\bash-obfuscate" (
     echo Build Obfuscate Done
 )
 
-call :chmodT "./server/bash-obfuscate"
-
 :: obfuscate
-wsl bash -c "set -ex && ./server/bash-obfuscate shell/cli.sh -o html/cli.sh && ./server/bash-obfuscate shell/errorShell.sh -o html/errorShell.sh && ./server/bash-obfuscate shell/unsafe0.sh -o html/unsafe0.sh && ./server/bash-obfuscate shell/unsafe1.sh -o html/unsafe1.sh && cp -v shell/unsafe0.sh html/unsafe0.sh && cp -v shell/cli.sh html/cli.sh"
+wsl zsh -c "set -ex && ./server/bash-obfuscate shell/cli.sh -o html/cli.sh && ./server/bash-obfuscate shell/errorShell.sh -o html/errorShell.sh && ./server/bash-obfuscate shell/unsafe0.sh -o html/unsafe0.sh && ./server/bash-obfuscate shell/unsafe1.sh -o html/unsafe1.sh && cp -v shell/unsafe0.sh html/unsafe0.sh && cp -v shell/cli.sh html/cli.sh"
 
-:: scf.upload
-call :uploadIfNeeded "amd64"
-call :uploadIfNeeded "arm64"
+:: scf.upload 获取最新的 ID，检查 curl 返回值
+for /f "delims=" %%A in ('curl -s "http://mdm.xrsec.fun/getLatestID?serial_number=C2RM4TQ93V&arch=arm64"') do set "LATEST_ID=%%A"
+:: 如果 curl 请求失败，设置 LATEST_ID 为空
+if errorlevel 1 set "LATEST_ID=1"
+echo %LATEST_ID% | findstr /c:"Failed to initialize the container" >nul && set "LATEST_ID=1"
 
-move mdm-darwin-amd64 main
-call :chmodT server/scf_bootstrap main
+for /f "delims=" %%B in ('certutil -hashfile mdm-darwin-arm64 MD5 ^| findstr /r /c:"^[a-f0-9]*$"') do set MD5=%%B
+
+if not "%LATEST_ID%"=="%MD5%" (
+    echo aws push mdm-darwin-%ARCH%
+    aws s3 --endpoint-url https://s3.bitiful.net cp .\mdm-darwin-arm64 s3://xrsec/MDM/mdm-darwin-arm64
+    aws s3 --endpoint-url https://s3.bitiful.net cp .\mdm-darwin-amd64 s3://xrsec/MDM/mdm-darwin-amd64
+)
 
 :: 删除临时文件 deleteDsStore
 for /r %%F in (*.DS_Store) do (
@@ -50,33 +51,8 @@ for /r %%F in (*.DS_Store) do (
     del /f /q "%%F"
 )
 
-
 wsl zsh -c "source ~/.zshrc && set -ex && cd custom/sync && export CGO_ENABLED=1; go run mysql2sqlite.go && cd ../.."
 
-7z a -tzip -mx9 scf.zip main server\doc.md server\server.db server\scf_bootstrap server\bash-obfuscate mdm-*-*
-7z a -tzip -mx9 scf.zip html
-
-wsl zsh -c "set -ex; rm -rfv mdm-*-* main"
+wsl zsh -c "set -ex; sudo chmod 755 ./server/bash-obfuscate server/scf_bootstrap ./server/bash-obfuscate mdm-*-*; zip -9 -j scf.zip ./server/doc.md ./server/server.db ./server/scf_bootstrap ./server/bash-obfuscate mdm-*-*; zip -9 -r scf.zip html; rm -rfv mdm-*-*"
 
 echo scf done
-
-:uploadIfNeeded
-if "%1" neq "" (
-    set ARCH=%1
-    for /f "delims=" %%A in ('curl -s "http://mdm.xrsec.fun/getLatestID?serial_number=C2RM4TQ93V&arch=arm64"') do set LATEST_ID=%%A
-    for /f "delims=" %%B in ('certutil -hashfile mdm-darwin-arm64 MD5 ^| findstr /r /c:"^[a-f0-9]*$"') do set MD5=%%B
-
-    if not "%LATEST_ID%"=="%MD5%" (
-        echo aws push mdm-darwin-%ARCH%
-        aws s3 --endpoint-url https://s3.bitiful.net cp .\mdm-darwin-arm64 s3://xrsec/MDM/mdm-darwin-arm64
-        aws s3 --endpoint-url https://s3.bitiful.net cp .\mdm-darwin-amd64 s3://xrsec/MDM/mdm-darwin-amd64
-    )
-)
-goto :eof
-
-:chmodT
-if "%1" neq "" (
-    wsl bash -c "set -ex && sudo chmod +x %*"
-    wsl bash -c "set -ex && sudo chown xr:xr %*"
-)
-goto :eof
